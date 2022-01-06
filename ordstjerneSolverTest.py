c9c0f47925a3f6a4453c5b6c78c60f937a6b1063
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-01-06 18:36:56 +0100

    Created a test file for the ordstjerneSolver script

---
+++ ordstjerneSolverTest.py
@@ -0,0 +1,22 @@
+from ordstjerneSolver import ordstjerne_solver
+import unittest
+import pandas as pd
+
+# Constants
+FULLFORMSLISTE_PATH = "fullformsliste.csv"
+COLUMN_TAG_OPPSLAG = "OPPSLAG"
+ALLOWED_LETTERS = ["i", "o", "u", "ø", "t", "n", "k"]
+MUST_HAVE = "k"
+
+# Dictionary
+fullformsliste = pd.read_csv(FULLFORMSLISTE_PATH)
+
+
+class TestOrdStjerneSolver(unittest.TestCase):
+    def test_ordstjerne_solver(self):
+        result = ordstjerne_solver(ALLOWED_LETTERS, MUST_HAVE)
+        self.assertEqual(len(result), 49)
+
+
+if __name__ == "__main__":
+    unittest.main()
