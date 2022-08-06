5d87397335d1c562ad0feb9f7ea193388f39b878
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-08-06 21:44:04 +0200

    made api accessible cross origin

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, make_response, request
 
+from flask_cors import CORS
+
 from ordstjerneSolver import ordstjerne_solver
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/solve', methods=['POST'])
